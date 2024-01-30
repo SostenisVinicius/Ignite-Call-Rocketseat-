2eece969ba163ff887b187635abd7e4674225a25
Sóstenis Vinícius <Socratis__@hotmail.com>
2024-01-29 23:46:08 -0300

    add: Using react hook form to control the ClaimUsernameForm

--- src/pages/home/components/ClaimUsernameForm/index.tsx
+++ src/pages/home/components/ClaimUsernameForm/index.tsx
@@ -1,14 +1,31 @@
 import { Button, TextInput } from '@ignite-ui/react'
-import { Form } from './styles'
 import { ArrowRight } from 'phosphor-react'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+import { Form } from './styles'
+
+const claimUsernameFormSchema = z.object({
+  username: z
+    .string()
+    .min(3, { message: 'O usuário precisa ter pelo menos 3 letras.' }),
+})
+
+type ClaimUsernameFormData = z.infer<typeof claimUsernameFormSchema>
 
 export function ClaimUsernameForm() {
+  const { register, handleSubmit } = useForm<ClaimUsernameFormData>()
+
+  async function handleClaimUsername(data: any) {
+    console.log(data)
+  }
+
   return (
-    <Form as="form">
+    <Form as="form" onSubmit={handleSubmit(handleClaimUsername)}>
       <TextInput
         size="sm"
         prefix="ignite.com/"
         placeholder="seu-usuario"
+        {...register('username')}
         crossOrigin=""
       />
       <Button size="sm" type="submit">
