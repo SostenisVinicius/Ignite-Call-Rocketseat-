97b5812db16f6c3a8c1e650903087a76b3a9e99e
Sóstenis Vinícius <Socratis__@hotmail.com>
2024-06-20 13:53:24 -0300

    fix: blocked-dates api and confirmStep are fixed, and create the schedule route

--- src/pages/api/users/[username]/blocked-dates.api.ts
+++ src/pages/api/users/[username]/blocked-dates.api.ts
@@ -38,13 +38,13 @@ export default async function handler(
 
   const blockedWeekDays = [0, 1, 2, 3, 4, 5, 6].filter((weekDay) => {
     return !availableWeekDays.some(
-      (availableWeekDays) => availableWeekDays.week_day === weekDay,
+      (availableWeekDay) => availableWeekDay.week_day === weekDay,
     )
   })
 
-  const blockedDatesRaw: Array<{ day: number }> = await prisma.$queryRaw`
+  const blockedDatesRaw: Array<{ date: number }> = await prisma.$queryRaw`
     SELECT
-      EXTRACT(DAY FROM S.date) AS day,
+      EXTRACT(DAY FROM S.DATE) AS date,
       COUNT(S.date) AS amount,
       ((UTI.time_end_in_minutes - UTI.time_start_in_minutes) / 60) AS size
 
@@ -52,16 +52,17 @@ export default async function handler(
 
     LEFT JOIN user_time_intervals UTI
       ON UTI.week_day = WEEKDAY(DATE_ADD(S.date, INTERVAL 1 DAY))
-      AND UTI.user_id = S.user_id
 
     WHERE S.user_id = ${user.id}
-      AND EXTRACT(YEAR from S.date) = ${year}
-      AND EXTRACT(MONTH from S.date) = ${month}
-    
-    GROUP BY day, size
+      AND DATE_FORMAT(S.date, "%Y-%m") = ${`${year}-${month}`}
+
+    GROUP BY EXTRACT(DAY FROM S.DATE),
+      ((UTI.time_end_in_minutes - UTI.time_start_in_minutes) / 60)
+
+    HAVING amount >= size
   `
 
-  const blockedDates = blockedDatesRaw.map((item) => item.day)
+  const blockedDates = blockedDatesRaw.map((item) => item.date)
 
   return res.json({ blockedWeekDays, blockedDates })
 }
